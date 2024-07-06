8b5d94659905fe4e738bc5b43fbffb82f040d5e8
Aron Resty Ramillano <abramillano@up.edu.ph>
2024-07-06 15:51:29 +0800

    fix: script now stops the first uvicorn instance

--- stop_fastapi_background.sh
+++ stop_fastapi_background.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Search for the Uvicorn process
-PID=$(ps aux | grep -m 1 "nohup uvicorn" | awk '{print $2}')
+# Command to start the FastAPI application using Uvicorn
+START_COMMAND="/usr/bin/python3 /usr/local/bin/uvicorn main:app --reload"
+
+# Search for the Uvicorn process using the exact start command
+PID=$(ps aux | grep -F "$START_COMMAND" | grep -v grep | awk '{print $2}')
 
 # Check if PID was found
 if [ -z "$PID" ]; then
